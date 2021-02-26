1151593db7af90f9c5cb4fa36ae43b030ff33dbf
unknown <jonathangobiel13@gmail.com>
2021-02-26 19:35:33 +0700

    add axios default authorization

--- src/_axios.js
+++ src/_axios.js
@@ -1,5 +1,15 @@
 import Axios from 'axios'
+import { KEY } from './constants/key'
 
 export const _axios = Axios.create({
-    baseURL : process.env.REACT_APP_BASE_URL,
+    baseURL: process.env.REACT_APP_BASE_URL,
 })
+
+_axios.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem(KEY.AUTH)
+        if (token)
+            config.headers['Authorization'] = `Bearer ${token}`
+        return config;
+    }, err => err
+)
